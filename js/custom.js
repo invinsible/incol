6a8eb286f9aeff37a07c8a0b6df12291aeb66b48
Invinsible <igor@tomatstudio.ru>
2019-05-07 08:16:07 +0300

    Десктоп версия 2 блоков с каруселью

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,34 @@
 $(document).ready(function(){
-  $('.multiple-items').slick({
+  $('.in_stories-list').slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
     dots: true,
+    arrows: true,
+    responsive: [
+    {
+    	breakpoint: 750,
+    	settings: {
+    		slidesToShow: 1
+    	}
+    }    	
+    ]
+  });
+
+  $('.in_team-list').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
     arrows: true
+  });
+
+  $('.in_works-list').slick({
+  	dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
   });
 });
 
